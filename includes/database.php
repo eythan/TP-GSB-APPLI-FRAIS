fc3d9b0a8414d5268337e60e23e0c04d2d2f31db
Eythan Franceus <eythan.franceus@gmail.com>
2025-03-13 21:45:31 +0100

    Update global

---
+++ includes/database.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $dbname = "mabase";
+    $username = "root";
+    $password = "";
+
+    try { 
+        $db= new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        echo "<span style='color: green;'>Connexion réussie à la base de données</span>";
+    } catch ( Exception $e ) { 
+        die("<span style='color: red;'>Connexion impossible avec la base de données : ".$e->getMessage()."</span>");
+    }
+?>
---
+++ public/javascript/script.js
@@ -0,0 +1,37 @@
+function ajoutLigne(pNumero) {
+    document.getElementById("but" + pNumero).setAttribute("hidden", "true");  
+    pNumero++;
+
+    var laDiv = document.getElementById("lignes");
+
+    var label = document.createElement("label");
+    label.className = "titre";
+    label.innerHTML = " " + pNumero + " : ";
+    laDiv.appendChild(label);
+
+    var ladate = document.createElement("input");
+    ladate.name = "FRA_AUT_DAT" + pNumero;
+    ladate.size = "12";
+    ladate.className = "zone";
+    laDiv.appendChild(ladate);
+
+    var libelle = document.createElement("input");
+    libelle.name = "FRA_AUT_LIB" + pNumero;
+    libelle.size = "30";
+    libelle.className = "zone";
+    laDiv.appendChild(libelle);
+
+    var mont = document.createElement("input");
+    mont.name = "FRA_AUT_MONT" + pNumero;
+    mont.size = "3";
+    mont.className = "zone";
+    laDiv.appendChild(mont);
+
+    var bouton = document.createElement("input");
+    bouton.type = "button";
+    bouton.value = "+";
+    bouton.className = "zone";
+    bouton.id = "but" + pNumero;
+    bouton.setAttribute("onclick", "ajoutLigne(" + pNumero + ");");
+    laDiv.appendChild(bouton);
+}
