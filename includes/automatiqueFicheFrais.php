cbd779afe26e0c33b7e6a4f7b60727982e473db4
Eythan Franceus <eythan.franceus@gmail.com>
2025-03-15 16:35:44 +0100

    Add automatiqueFicheFrais.php

---
+++ includes/automatiqueFicheFrais.php
@@ -0,0 +1,29 @@
+<?php
+    // Script de connexion BDD
+    include("database.php");
+
+    // Récupération de la date du jour
+    $datefrais = date("d");
+    $mois = date("m");
+    $annee = date("Y");
+
+    if($datefrais == 15) {
+        // Écriture de la requête SQL
+        $selectSQL = "SELECT id_utilisateur FROM utilisateurs";
+
+        // Envoie de la requête, stockage dans $result
+        $result = $db->query($selectSQL);
+
+        // Effectuer le traitement sur chaque utilisateur
+        foreach ($result as $ligne) {
+            // Récupération de l'id de l'utilisateur
+            $id_utilisateur = $ligne['id_utilisateur'];
+
+            // Écriture de la requête SQL
+            $insertSQL = "INSERT INTO fiche_frais (id_utilisateur, mois, annee, id_etat) VALUES ($id_utilisateur, $mois, $annee, 1)";
+
+            // Envoie de la requête
+            $db->exec($insertSQL);
+        }
+    }
+?>
